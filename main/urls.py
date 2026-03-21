fa666802f06c00f62184a71dab05bed83b68f75a
Madalitso Tembo <tembomadalito9@gmail.com>
2026-03-21 16:15:48 +0200

    urls issue resolved

--- main/urls.py
+++ main/urls.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
-from django.urls import path, include 
-<<<<<<< HEAD
-from core.views import index_page, auth_page, add_property_page, dashboard_page, property_detail_page, official_dashboard_page, apply_page
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-=======
-from django.conf import settings
-from django.conf.urls.static import static
-from core.views import index_page, auth_page, add_property_page, dashboard_page, property_detail_page, zra_dashboard_page, occupancy_dashboard_page, apply_page
->>>>>>> feature/simplot-proptech-platform-4136760008759981879
+from core.views import (
+    index_page,
+    auth_page,
+    add_property_page,
+    dashboard_page,
+    property_detail_page,
+    zra_dashboard_page,
+    occupancy_dashboard_page,
+    apply_page,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_page, name='index'), # Serves the integrated home page
-    path('api/', include('core.urls')), # Points to core/urls.py for API
+    path('', index_page, name='index'),
+    path('api/', include('core.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/login/', auth_page, name='login'),
@@ -22,12 +25,8 @@ urlpatterns = [
     path('property/<int:pk>/', property_detail_page, name='property-detail'),
     path('gov/zra/', zra_dashboard_page, name='zra-dashboard'),
     path('gov/occupancy/', occupancy_dashboard_page, name='occupancy-dashboard'),
-    path('property/<int:pk>/apply/', apply_page, name='apply-property')
-<<<<<<< HEAD
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
+    path('property/<int:pk>/apply/', apply_page, name='apply-property'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> feature/simplot-proptech-platform-4136760008759981879
